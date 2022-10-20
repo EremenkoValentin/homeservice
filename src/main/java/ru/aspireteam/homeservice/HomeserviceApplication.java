f6ab9a31d1359394cfc9174880421d44671e8a6b
Eremenko Valentin <v.u.eremenko@yandex.ru>
2022-10-20 18:18:56 +0300

    Initial commit

---
+++ src/main/java/ru/aspireteam/homeservice/HomeserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.aspireteam.homeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aspireteam/homeservice/HomeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aspireteam.homeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
